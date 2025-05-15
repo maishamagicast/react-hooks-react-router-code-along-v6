952bcd3e12ad21a9d393da1ba4f3c39457c13f61
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-15 19:29:14 +0300

    okay

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+    return(
+        <nav>
+        <NavLink
+            to="/"
+            /* add styling to Navlink */
+            className="nav-link"
+        >
+            Home
+        </NavLink>
+        <NavLink
+            to='/about'
+            className="nav-link">
+            About
+        </NavLink>
+        <NavLink
+            to='/Login'
+            className='nav-link'>
+            Login
+        </NavLink>
+        </nav>
+      
+      
+    )
+}
+export default NavBar;
